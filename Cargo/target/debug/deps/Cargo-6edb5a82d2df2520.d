603b346991e0a2cf84f80a4d7fd6f19300dbc609
marud <maruder140@gmail.com>
2026-03-01 00:50:53 +0100

    Added the design directory and initial cargo

---
+++ Cargo/target/debug/deps/Cargo-6edb5a82d2df2520.d
@@ -0,0 +1,5 @@
+C:\Users\marud\RustroverProjects\BKVNengine\Cargo\target\debug\deps\libCargo-6edb5a82d2df2520.rmeta: src\main.rs
+
+C:\Users\marud\RustroverProjects\BKVNengine\Cargo\target\debug\deps\Cargo-6edb5a82d2df2520.d: src\main.rs
+
+src\main.rs:
---
+++ Cargo/target/debug/deps/Cargo-d2f77a655cd6fdb8.d
@@ -0,0 +1,5 @@
+C:\Users\marud\RustroverProjects\BKVNengine\Cargo\target\debug\deps\libCargo-d2f77a655cd6fdb8.rmeta: src\main.rs
+
+C:\Users\marud\RustroverProjects\BKVNengine\Cargo\target\debug\deps\Cargo-d2f77a655cd6fdb8.d: src\main.rs
+
+src\main.rs:
