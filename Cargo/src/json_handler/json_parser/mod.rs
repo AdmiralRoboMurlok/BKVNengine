e6e7113f87b497716d651375bcc7695cfd1e6231
marud <maruder140@gmail.com>
2026-04-03 22:32:24 +0200

    This code should create the FirstProjectJson

--- Cargo/src/json_handler/json_parser/mod.rs
+++ Cargo/src/json_handler/json_parser/mod.rs
@@ -4,7 +4,7 @@ use crate::json_handler::json_struct::{ProjectJSON, Scene};
 
 pub fn create_scene(scene_id: u32, number_of_characters_in_scene: u8,
                     characters: Vec<String>, background: Vec<String>,
-                    sound: Vec<String>) -> serde_json::Result<Scene> {
+                    sound: Vec<String>) -> serde_json::error::Result<String> {
     let scene: &str = &format!(r#"
     {{
         id: {:?},
@@ -16,13 +16,13 @@ pub fn create_scene(scene_id: u32, number_of_characters_in_scene: u8,
     "#,
     scene_id, number_of_characters_in_scene, characters, background, sound);
 
-    let scene_json: serde_json::Result<Scene> = from_str::<Scene>(scene);
+    let scene_json = serde_json::to_string_pretty(scene);
     scene_json
 }
 
 pub fn create_project(name: &str, last_change: SystemTime, characters: Vec<String>,
                   backgrounds: Vec<String>, sounds: Vec<String>,
-                  scene: serde_json::error::Result<Scene>) -> serde_json::Result<ProjectJSON> {
+                  scene: serde_json::error::Result<String>) -> serde_json::error::Result<String> {
     let project: &str = &format!(r#"
     {{
         name: {:?},
@@ -35,6 +35,6 @@ pub fn create_project(name: &str, last_change: SystemTime, characters: Vec<Strin
     "#,
     name, last_change, characters, backgrounds, sounds, scene);
 
-    let project_json = from_str::<ProjectJSON>(project);
+    let project_json =  serde_json::to_string_pretty(project);
     project_json
 }
--- Cargo/src/json_handler/mod.rs
+++ Cargo/src/json_handler/mod.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Write;
 use crate::json_handler::json_parser::{create_scene, create_project};
 use crate::json_handler::json_struct::{ProjectJSON, Scene};
 use std::time::SystemTime;
@@ -7,13 +9,16 @@ mod json_parser;
 pub mod json_struct;
 
 pub fn create_first_project_json() {
-    let scene: serde_json::Result<Scene> = create_scene(0, 1, Vec::new(), Vec::new(), Vec::new());
+    let scene: serde_json::error::Result<String> = create_scene(0, 1, Vec::new(), Vec::new(), Vec::new());
 
     let date_now = SystemTime::now();
 
-    let project: serde_json::Result<ProjectJSON> = create_project("FirstProject", date_now, Vec::new(), Vec::new(), Vec::new(), scene);
+    let project_data = create_project("FirstProject", date_now, Vec::new(), Vec::new(), Vec::new(), scene);
+
+    let mut json_file = File::create("./project/ProjectData.json");
+    json_file.unwrap().write_all(project_data.unwrap().as_bytes());
 }
 
-pub fn create_project_json() {
+pub fn create_project_json(ProjectName: String) {
     
 }
